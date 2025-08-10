a6dc9601e33d3e63eb4f44767311ceccf23db10b
Islam Mamedov <dexterjamescole@gmail.com>
2025-08-10 14:55:54 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import orderRoutes      from './routes/orderRoutes.js';
 import modifierRoutes from './routes/modifierRoutes.js';
 const app = express();
 app.use(cors());
-app.use('/api/modifiers', modifierRoutes);
+
 app.use(morgan('dev'));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
@@ -22,7 +22,7 @@ app.use('/api/restaurants', restaurantRoutes);
 app.use('/api/menu',      menuRoutes);
 app.use('/api/email',     emailRoutes);
 app.use('/api/orders',    orderRoutes);
-
+app.use('/api/modifiers', modifierRoutes);
 const PORT = 4000;
 app.listen(PORT, () =>
   console.log(`✅ Server running at http://localhost:${PORT}`)
