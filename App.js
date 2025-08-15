6f00545329dd54b7b3f193bddfd8d2bddaff8d31
Islam Mamedov <dexterjamescole@gmail.com>
2025-08-15 19:46:48 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
+// App.js
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
+
 import { RestaurantProvider } from './context/RestaurantContext';
 import { CartProvider } from './context/CartContext';
 import { OrderProvider } from './context/OrderContext';
+import { AuthProvider } from './context/AuthContext';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <RestaurantProvider>
-        <CartProvider>
-          <OrderProvider>
-            <NavigationContainer>
-              <AppNavigator />
-            </NavigationContainer>
-          </OrderProvider>
-        </CartProvider>
-      </RestaurantProvider>
+      <AuthProvider>
+        <RestaurantProvider>
+          <CartProvider>
+            <OrderProvider>
+              <NavigationContainer>
+                <AppNavigator />
+              </NavigationContainer>
+            </OrderProvider>
+          </CartProvider>
+        </RestaurantProvider>
+      </AuthProvider>
     </GestureHandlerRootView>
   );
 }
