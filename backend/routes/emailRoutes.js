950c630bc1626bdc746b3d426255b42de19b4ab6
Islam Mamedov <dexterjamescole@gmail.com>
2025-08-15 19:09:56 +0800

    Update emailRoutes.js

--- backend/routes/emailRoutes.js
+++ backend/routes/emailRoutes.js
@@ -1,8 +1,7 @@
+// backend/routes/emailRoutes.js
 import express from 'express';
 import { sendEmail } from '../controllers/emailController.js';
 
-const r = express.Router();
-
-r.post('/restaurants/:id/email', sendEmail); // backend route for sending email
-
-export default r;
+const router = express.Router();
+router.post('/send', sendEmail);
+export default router;
